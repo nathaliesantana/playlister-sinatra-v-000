2267fd60c37a76e5479d8243645852552bad3ba7
Nathalie Santana <nathalie.scruz@gmail.com>
2019-02-15 03:52:19 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -29,9 +29,11 @@ class SongsController < ApplicationController
   end
 
   get'/songs/:slug/edit' do
-
+    @song = Song.find_by_slug(params[:slug])
+    
+    erb:'/songs/edit'
   end
-  
+
 
 
 end
